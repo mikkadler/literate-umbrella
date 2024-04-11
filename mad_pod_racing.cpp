f91a4e7f5a45a5510da48c612d2dbe6ddc0ebf00
Mikk <adler.mikk@gmail.com>
2024-04-11 16:21:01 +0300

    silver -> gold

--- mad_pod_racing.cpp
+++ mad_pod_racing.cpp
@@ -2,15 +2,19 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#include <corecrt_math_defines.h>
 
 using namespace std;
+
+double getAngle(int delta_x, int delta_y);
 
 int main()
 {
 
 int previous_tick_checkpoint_distance = -1;
 int boost_left = 1;
-const int angle_to_start_throttle = 70;
+const int angle_to_start_throttle = 120;
 const double time_to_no_throttle = 4.5;
 int prev_tick_x = -1;
 int perv_tick_y = -1;
@@ -33,6 +37,7 @@ int perv_tick_y = -1;
 
         int thrust = 100;
         bool boost = false;
+        bool shield = false;
 
         if (previous_tick_checkpoint_distance < 0) {
             // first tick
@@ -50,23 +55,29 @@ int perv_tick_y = -1;
 
         
 
-        if (boost_left > 0 && next_checkpoint_dist > 5000 && (next_checkpoint_angle > 3 || next_checkpoint_angle < -3) && speed > 0 ) {
+        if (boost_left > 0 && next_checkpoint_dist > 5000 && (next_checkpoint_angle < 3 || next_checkpoint_angle > -3) && speed > 0 ) {
             boost = true;
             boost_left--;
         }        
 
         cerr << "Speed:" << speed << " dist to next:" << next_checkpoint_dist << std::endl;
         cerr << "Speed x:" << speed_x << " speed y:" << speed_y << std::endl;
-
+/*
+        double movement_angle = getAngle(speed_x, speed_y);
+        int delta_w_x = next_checkpoint_x - x;
+        int delta_w_y = next_checkpoint_y - y;
+        double waypoint_angle = getAngle(delta_w_x, delta_w_y);
+        cerr << "move angle:" << movement_angle << " next waypoint angle:" << waypoint_angle << std::endl;
+*/
         if (speed * time_to_no_throttle > next_checkpoint_dist) {
-            thrust = 0;
+            thrust = 40;
         }
         if (next_checkpoint_angle > angle_to_start_throttle || next_checkpoint_angle < (-1 * angle_to_start_throttle)) {
-            thrust = 0;
+            thrust = 40;
         }
 
-        next_checkpoint_x = next_checkpoint_x - speed_x;
-        next_checkpoint_y = next_checkpoint_y - speed_y;
+        next_checkpoint_x = next_checkpoint_x - speed_x * 1.5;
+        next_checkpoint_y = next_checkpoint_y - speed_y * 1.5;
 
 
         // You have to output the target position
@@ -74,8 +85,16 @@ int perv_tick_y = -1;
         // i.e.: "x y thrust"
         if (boost) {
             cout << next_checkpoint_x << " " << next_checkpoint_y << " BOOST" << endl;
-        }else {
+        } else if (shield) {
+            cout << next_checkpoint_x << " " << next_checkpoint_y << " SHIELD" << endl;
+        } else {
             cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
         }
     }
+
 }
+
+double getAngle(int delta_x, int delta_y) 
+{
+        return atan2(delta_y, delta_x) * 180 / M_PI;
+};
